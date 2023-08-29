73b2eb5d778fe3799d29e3315b1f41ea4e2523c4
noskov <olega6000@gmail.com>
2023-08-29 22:50:35 +0300

    Add ConfigProperties variables from application file

--- wf-core-services/src/main/java/com/service/core/WfCoreServicesApplication.java
+++ wf-core-services/src/main/java/com/service/core/WfCoreServicesApplication.java
@@ -1,9 +1,12 @@
 package com.service.core;
 
+import com.service.core.config.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ConfigProperties.class)
 public class WfCoreServicesApplication {
 
     public static void main(String[] args) {
--- wf-core-services/src/main/java/com/service/core/controller/WeatherController.java
+++ wf-core-services/src/main/java/com/service/core/controller/WeatherController.java
@@ -1,10 +1,10 @@
 package com.service.core.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.service.core.config.ConfigProperties;
 import com.service.core.entity.Forecast;
 import com.service.core.service.ForecastService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,22 +18,21 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class WeatherController {
     private final ForecastService forecastService;
-
-    @Value("${weather.api.key}")
-    private String weatherKey;
-
-    @Value("${weather.api.url}")
-    private String weatherAPI;
+    private final ConfigProperties configProperties;
 
     @GetMapping("/weather/countries/{country}/cities/{city}")
     public Forecast getWeatherByCityAndCountry(@PathVariable String country,
                                                @PathVariable String city) throws JsonProcessingException, ParseException {
+        String path = "/v1/current.json?key=%s&q=%s&q=%s";
+        String weatherUrl =
+                String.format(configProperties.getApi() + path, configProperties.getKey(), country, city);
+
         RestTemplate restTemplate = new RestTemplate();
-        String weatherUrl = String.format(weatherAPI, weatherKey, country, city);
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         HttpEntity<?> requestEntity = new HttpEntity<>(headers);
         ResponseEntity<String> response = restTemplate.exchange(weatherUrl, HttpMethod.GET, requestEntity, String.class);
+
         return forecastService.getForecast(response.getBody());
     }
 }
