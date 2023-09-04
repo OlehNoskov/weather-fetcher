dd7a9f5c9f1b55254ca0416c2097c5515bd6ed5e
noskov <olega6000@gmail.com>
2023-09-04 22:01:34 +0300

    Add wf-stats-services module.

---
+++ wf-stats-services/src/main/java/com/stats/WfStatsServicesApplication.java
@@ -0,0 +1,12 @@
+package com.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfStatsServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfStatsServicesApplication.class, args);
+    }
+}
