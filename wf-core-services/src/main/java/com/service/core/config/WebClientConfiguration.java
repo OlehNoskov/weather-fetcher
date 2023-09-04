a35ed4994d45965f88b53de9dd564833f4a0318a
noskov <olega6000@gmail.com>
2023-09-04 13:35:38 +0300

    Moved ignore file and add Apache cxf path

--- wf-core-services/src/main/java/com/service/core/config/WebClientConfiguration.java
+++ wf-core-services/src/main/java/com/service/core/config/WebClientConfiguration.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.cxf.Bus;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,13 +16,15 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 public class WebClientConfiguration {
+    @Value("${cxf.path}")
+    private String cxfPath;
 
     @Bean
     public Server jaxrsServer(Bus bus, WeatherControllerImpl weatherController) {
         JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
         endpoint.setBus(bus);
         endpoint.setServiceBeans(Collections.singletonList(weatherController));
-        endpoint.setAddress("/");
+        endpoint.setAddress(cxfPath);
         endpoint.setProviders(List.of(new ForecastMessageBodyWriter()));
         return endpoint.create();
     }
