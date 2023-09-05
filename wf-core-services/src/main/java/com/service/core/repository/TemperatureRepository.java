04cf4a0c1b07682ccdb475db2b690eadecf84b46
noskov <olega6000@gmail.com>
2023-09-05 18:02:53 +0300

    Add init sql file for database.

--- wf-core-services/src/main/java/com/service/core/repository/TemperatureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.service.core.repository;
-
-import com.service.core.entity.Temperature;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TemperatureRepository extends CrudRepository<Temperature, Long> {
-}
