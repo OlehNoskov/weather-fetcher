aa9b974c4ee4cff687cf508fceacdce49f223b5b
noskov <olega6000@gmail.com>
2023-09-06 10:38:31 +0300

    Minor changes

---
+++ sql/init.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS temperature;
+DROP TABLE IF EXISTS weather;
+DROP TABLE IF EXISTS forecast;
+
+CREATE TABLE temperature
+(
+    id      SERIAL PRIMARY KEY,
+    scale   INTEGER NOT NULL,
+    degrees INTEGER NOT NULL
+);
+
+CREATE TABLE weather
+(
+    id             SERIAL PRIMARY KEY,
+    overall        INTEGER NOT NULL,
+    temperature_id INTEGER NOT NULL
+);
+
+CREATE TABLE forecast
+(
+    id         SERIAL PRIMARY KEY,
+    country    VARCHAR(100) NOT NULL,
+    city       VARCHAR(100) NOT NULL,
+    date       TIMESTAMP    NOT NULL,
+    weather_id INTEGER      NOT NULL
+);
